e425d1cff2e33e8d3c37994cc88aec9b4f0a7dda
ahmedbmhrz <ahmed.bamuhriz@gmail.com>
2025-02-01 12:50:54 +0300

    Create About Section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,76 @@
+import React, { PureComponent } from "react";
+import "./About.css";
+import fireAbout from "../../assets/fire-About.png";
+import draAbout from "../../assets/dragon-About.png";
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-title">
+        <h1>About me</h1>
+        <img src={fireAbout} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img className="imgAbout" src={draAbout} alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              This program is designed for developers who want to excel in both
+              front-end development and C++. It provides hands-on experience
+              with modern web technologies such as HTML, CSS, and JavaScript,
+              along with in-depth knowledge of C++ for building efficient and
+              high-performance applications.
+            </p>
+            <p>
+              Whether you're looking to master UI/UX design, responsive web
+              development, or advanced C++ programming concepts like data
+              structures, algorithms, and system-level programming, this program
+              offers the perfect blend of both worlds.
+            </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>JavaScript</p>
+              <hr style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>C++</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>C</p>
+              <hr style={{ width: "45%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Java</p>
+              <hr style={{ width: "70%" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>7</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>5</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
