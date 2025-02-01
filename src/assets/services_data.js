a9910b527a784242d681590e55ef9be9a0bcf3a4
ahmedbmhrz <ahmed.bamuhriz@gmail.com>
2025-02-01 18:26:30 +0300

    Create Services Section

---
+++ src/assets/services_data.js
@@ -0,0 +1,55 @@
+const Services_data = [
+    {
+      S_on: 1,
+      S_name: "HTML & CSS Development",
+      S_desc: "Building responsive and visually appealing web pages using HTML and CSS."
+    },
+    {
+      S_on: 2,
+      S_name: "React Development",
+      S_desc: "Creating dynamic and interactive web applications using React.js."
+    },
+    {
+      S_on: 3,
+      S_name: "C++ Programming",
+      S_desc: "Developing high-performance applications with C++ programming language."
+    },
+    {
+      S_on: 4,
+      S_name: "C Programming",
+      S_desc: "Building efficient and low-level applications using the C language."
+    },
+    {
+      S_on: 5,
+      S_name: "JavaScript Development",
+      S_desc: "Enhancing web interactivity and functionality using JavaScript."
+    },
+    {
+      S_on: 6,
+      S_name: "Java Development",
+      S_desc: "Creating robust and scalable applications using Java programming language."
+    },
+    {
+      S_on: 7,
+      S_name: "Front-end Development",
+      S_desc: "Designing and implementing user interfaces with modern web technologies."
+    },
+    {
+      S_on: 8,
+      S_name: "Back-end Development",
+      S_desc: "Developing server-side applications and APIs for web platforms."
+    },
+    {
+      S_on: 9,
+      S_name: "Full-stack Development",
+      S_desc: "Integrating front-end and back-end technologies for complete applications."
+    },
+    {
+      S_on: 10,
+      S_name: "Software Engineering",
+      S_desc: "Applying software development principles to create high-quality applications."
+    }
+  ];
+  
+  export default Services_data;
+  
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,35 @@
+import React, { PureComponent } from 'react';
+import './Services.css'
+import theme_pattenrn from '../../assets/fire-About.png'
+import Services_Data from '../../assets/services_data.js'
+import arrow_icon from "../../assets/arrow_icon.png"
+const Services = () =>{
+    return (
+        <div className='services'>
+            <div className="services-title">
+                <h1>My Services</h1>
+                <img src={theme_pattenrn} alt="" />
+            </div>
+            <div className="services-container">
+                {Services_Data.map((services,index)=>{
+                    return <div key={index} className="services-format">
+                        <h3>{services.S_on}</h3>
+                        <h2>{services.S_name}</h2>
+                        <p>{services.S_desc}</p>
+                        <div className="services-readmore">
+                            <p>Read More </p>
+                            <img src={arrow_icon} alt="" />
+                        </div>
+
+                    </div>
+
+                })}
+
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Services;
